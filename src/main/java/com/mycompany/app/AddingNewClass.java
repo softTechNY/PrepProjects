3c239fb0c9a70b25f7e61784ab35018d75bd0940
mdhossain62 <mhossain456@gmail.com>
2017-05-26 01:44:00 -0400

    initial commit

---
+++ src/main/java/com/mycompany/app/AddingNewClass.java
@@ -0,0 +1,8 @@
+package com.mycompany.app;
+
+
+public class AddingNewClass {
+	public static void printMessage(String message){
+	System.out.println(message);
+	}
+}
---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AddingNewClass.printMessage( "My name is Shamim" + "Let's go bang bang" );
+        
+    }
+}
